bc5ad751b1b86fa05193581433edb5927f1a7d12
HaseebAhmed49 <haseebahmed02@gmail.com>
2022-09-28 12:39:11 +0500

    first commit

---
+++ DatingApp.API/DatingApp.API/Data/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using DatingApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Data
+{
+    public class DataContext: DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options):base(options) {}
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
+
---
+++ DatingApp.API/DatingApp.API/Models/Value.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace DatingApp.API.Models
+{
+    public class Value
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
+
