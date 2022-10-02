26601588a089cee02d8a0e5da156f401594f9f13
HaseebAhmed49 <haseebahmed02@gmail.com>
2022-10-03 00:33:22 +0500

    Worked on Data Sharing between Parent to Child and Child to Parent Component

--- DatingApp.API/DatingApp.API/Controllers/ValuesController.cs
+++ DatingApp.API/DatingApp.API/Controllers/ValuesController.cs
@@ -24,6 +24,7 @@ namespace DatingApp.API.Controllers
         }
 
         // GET: api/values
+        [AllowAnonymous]
         [HttpGet]
         public async Task<IActionResult> Get()
         {
@@ -32,7 +33,6 @@ namespace DatingApp.API.Controllers
         }
 
         // GET api/values/5
-        [AllowAnonymous]
         [HttpGet("{id}")]
         public async Task<IActionResult> Get(int id)
         {
