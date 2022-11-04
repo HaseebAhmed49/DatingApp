f1891b5cc1f0985e31bc1660e26ce23440f2467c
HaseebAhmed49 <haseebahmed02@gmail.com>
2022-11-04 23:29:20 +0500

    Working on ngx-pipe

--- DatingApp.UI/src/app/app.module.ts
+++ DatingApp.UI/src/app/app.module.ts
@@ -29,7 +29,7 @@ import { PreventUnsavedChanges } from './_guards/prevent-unsaved-changes.guard';
 import { PhotoEditorComponent } from './members/photo-editor/photo-editor.component';
 import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import {NgPipesModule} from 'ngx-pipes';
 
 export function tokenGetter(){
   return localStorage.getItem('token');
@@ -47,7 +47,7 @@ export function tokenGetter(){
     MemberCardComponent,
     MemberDetailComponent,
     MemberEditComponent,
-    PhotoEditorComponent
+    PhotoEditorComponent,
    ],
   imports: [
     BrowserModule,
@@ -58,6 +58,7 @@ export function tokenGetter(){
     FileUploadModule,
     TabsModule.forRoot(),
     BsDatepickerModule.forRoot(),
+    NgPipesModule,
     RouterModule.forRoot(appRoutes),
     JwtModule.forRoot({
       config:{
